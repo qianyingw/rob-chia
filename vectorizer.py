1aa80fa7d452a2ce641c54efba39b9e92f1fd48b
Qianying Wang <w.qianying@outlook.com>
2020-04-03 23:55:14 +0100

    add d2v vectorizer

--- vectorizer.py
+++ vectorizer.py
@@ -9,6 +9,7 @@ Created on Thu Apr  2 14:25:27 2020
 
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from gensim.models import Doc2Vec
 
 #%% Bag-of-Words with/without TFIDF
 def bow(X_train, X_valid, ngram, min_df, max_f, use_tfidf):
@@ -44,3 +45,35 @@ def avg_w2v(w2v, X_train, X_valid, min_df, max_f):
 
 
 #%% Doc2Vec
+def d2v(train_tagged, valid_tagged, d2v_model, d2v_alpha, d2v_min_alpha, vector_size, min_count, max_vocab_size):
+    
+    if d2v_model == "dm":
+        model = Doc2Vec(dm=1,  # 1: PV-DM. 0: PV-DBOW
+                        dm_mean=1,  # 0: use the sum of the context word vectors. 1: use the mean
+                        epochs=20, seed=1234, workers=1,
+                        alpha=d2v_alpha, min_alpha=d2v_min_alpha,  # initial learning rate; linearly drops to min_alpha as training progresses                   
+                        vector_size=vector_size,  # feature vector dim
+                        min_count=min_count, max_vocab_size=max_vocab_size,
+                        hs=0,  # 1: hierarchical softmax is used
+                        negative=5)  # use negative sampling - how many “noise words” should be drawn
+        
+    if d2v_model == "dbow":
+        model = Doc2Vec(dm=0,  # 1: PV-DM. 0: PV-DBOW
+                        epochs=20, seed=1234, workers=1,
+                        alpha=d2v_alpha, min_alpha=d2v_min_alpha,  # initial learning rate; linearly drops to min_alpha as training progresses                   
+                        vector_size=vector_size,  # feature vector dim
+                        min_count=min_count, max_vocab_size=max_vocab_size,
+                        hs=0,  # 1: hierarchical softmax is used
+                        negative=5)  # use negative sampling - how many “noise words” should be drawn                      
+    
+    model.build_vocab(train_tagged)
+    model.train(train_tagged, total_examples=len(train_tagged.values), epochs=model.epochs)
+    
+    train_docs = train_tagged.values
+    Y_train, X_train_vec = zip(*[(doc.tags[0], model.infer_vector(doc.words, steps=20)) for doc in train_docs])
+
+    valid_docs = valid_tagged.values
+    Y_valid, X_valid_vec = zip(*[(doc.tags[0], model.infer_vector(doc.words, steps=20)) for doc in valid_docs])
+    
+    return X_train_vec, Y_train, X_valid_vec, Y_valid
+
